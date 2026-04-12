2d3a13b816fe4e7138ebef47764538e3781407db
Rohit Sahu <sahu.rohit2101@gmail.com>
2026-04-12 15:26:34 +0530

    Started Inner Classes

---
+++ 11. OOP/Inner_Classes/Nested_InnerClass.java
@@ -0,0 +1,24 @@
+package Inner_Classes;
+
+class Outer{
+    int x = 10;
+    class Inner{
+        int y = 20;
+        public void innerDisplay(){
+            System.out.println(x+" "+y);
+        }
+    }
+
+    public void outerDisplay(){
+        Inner i = new Inner();
+        i.innerDisplay();
+        System.out.println("y of Inner Class contains "+i.y);
+    }
+}
+
+public class Nested_InnerClass {
+    public static void main(String[] args){
+        Outer o = new Outer();
+        o.outerDisplay();
+    }
+}
